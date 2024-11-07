67dfa6d5c09171322ceabe4f1a51631699de23d4
maulidaprina <mldaprna@gmail.com>
2024-11-07 16:54:24 +0700

    modifikasi program

--- ArrayNilai12.java
+++ ArrayNilai12.java
@@ -7,16 +7,23 @@ public class ArrayNilai12 {
 
     int[] nilaiAkhir = new int [10];
     
-    for (int i = 0; i < 10; i++) {
-        System.out.print("Masukkan nilai akhir ke-" + i + ": ");
-        nilaiAkhir[i] = Input12.nextInt();
-
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+        System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            nilaiAkhir[i] = Input12.nextInt();
+    
     }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+        if (nilaiAkhir[i] > 70){
+            System.out.println("Mahasiswa ke-" + i + " lulus!");
+        } else { 
+            System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
         }
+    }
+    
 
 
    } 
 }
+
+
